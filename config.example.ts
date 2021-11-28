9b411003f421a2a9df570275a2cde7d8e99744d9
zvykach <zvykach.dev@gmail.com>
2021-11-28 04:44:59 +0200

    Example config added

---
+++ config.example.ts
@@ -0,0 +1,6 @@
+const config = {
+    "DS_TOKEN": "Di9cord-T0ken-1s-l0nger",
+    "MONGO_URI": "mongodb+srv://username:password@cluster-name.domain/db-name?retryWrites=true&w=majority"
+}
+
+export default config
