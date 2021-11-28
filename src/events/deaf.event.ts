b429486be266837317006ed13fd38cdfb7e0d24c
zvykach <zvykach.dev@gmail.com>
2021-11-28 02:26:46 +0200

    Deaf first

---
+++ src/events/deaf.event.ts
@@ -0,0 +1,47 @@
+import {IEvent} from "@/interfaces/IEvent";
+import {GuildService} from "@/services/guild.service";
+import {VoiceChannelService} from "@/services/voiceChannel.service";
+import {VoiceState} from "discord.js";
+import {VoiceChannelModel} from "@/models/voiceChannel.model";
+import {IUser} from "@/interfaces/IUser";
+
+const event: IEvent<"mutedInVoice"> = {
+    name: "mutedInVoice",
+    run: async (oldState: VoiceState, newState: VoiceState) => {
+        if (await GuildService.isPermanentlyMuted(newState.guild.id, newState.id)) {
+            await newState.setMute(true);
+            return;
+        }
+
+        if (! await VoiceChannelService.isPrivateChannel(newState.channelId!)) {
+            return;
+        }
+
+        const audit = await newState.guild.fetchAuditLogs({type:24, limit:1});
+        const entry = audit.entries.first();
+
+        if (!(entry && entry.executor && entry.target)) {
+            return;
+        }
+
+        const isMute = entry.changes?.find(change => change.key === 'mute' && change.new === true);
+
+        if (!isMute) {
+            return;
+        }
+
+        if (await VoiceChannelService.isChannelOwner(newState.channelId!, newState.id)) {
+            await newState.setMute(false);
+            return;
+        }
+
+        const toPush: IUser = {
+            who: newState.id,
+            by: entry.executor?.id
+        }
+
+        await VoiceChannelModel.findOneAndUpdate({channelId: newState.channelId!},{ $push: {mutedUsers: toPush}});
+    }
+}
+
+export default event;
---
+++ src/events/unDeaf.event.ts
@@ -0,0 +1,37 @@
+import {IEvent} from "@/interfaces/IEvent";
+import {GuildService} from "@/services/guild.service";
+import {VoiceChannelService} from "@/services/voiceChannel.service";
+import {VoiceState} from "discord.js";
+import {VoiceChannelModel} from "@/models/voiceChannel.model";
+import {IUser} from "@/interfaces/IUser";
+
+const event: IEvent<"unMutedInVoice"> = {
+    name: "unMutedInVoice",
+    run: async (oldState: VoiceState, newState: VoiceState) => {
+        if (await GuildService.isPermanentlyMuted(newState.guild.id, newState.id)) {
+            await newState.setMute(true);
+            return;
+        }
+
+        if (! await VoiceChannelService.isPrivateChannel(newState.channelId!)) {
+            return;
+        }
+
+        const audit = await newState.guild.fetchAuditLogs({type:24, limit:1});
+        const entry = audit.entries.first();
+
+        if (!(entry && entry.executor)) {
+            return;
+        }
+
+        const isMute = entry.changes?.find(change => change.key === 'mute' && change.new === false);
+
+        if (!isMute) {
+            return;
+        }
+
+        await VoiceChannelModel.findOneAndUpdate({channelId: newState.channelId!},{$pull: {mutedUsers: {who: newState.id}}});
+    }
+}
+
+export default event;
